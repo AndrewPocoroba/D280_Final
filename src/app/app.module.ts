e490017b63f46942533b85798cdda92e47c78c46
Andrew Pocoroba <programmingbyandrew@gmail.com>
2023-12-06 09:27:16 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapModule } from './map/map.module';
+import { TooltipModule } from './tooltip/tooltip.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MapModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
